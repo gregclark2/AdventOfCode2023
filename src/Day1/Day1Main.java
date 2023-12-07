d3de6391c2f2fd9c32f536f502c15e7f8d7da702
gclark2 <gclark2@my.chattanoogastate.edu>
2023-12-07 16:44:01 -0500

    day1 complete

--- src/Day1/Day1Main.java
+++ src/Day1/Day1Main.java
@@ -1,4 +1,44 @@
 package Day1;
 
-public class Day1Main {
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class Day1Main{
+    public static void main(String[] args){
+        int lineCounter = 1;
+        String line;
+        String numericLine;
+        int sum = 0;
+        try {
+            //filepath, filereader, scanner in
+            String filePath = "C:\\Users\\gregt\\IdeaProjects\\AdventofCode2023\\src\\Day1\\Day1Text.txt";
+            FileReader fr = new FileReader(filePath);
+            Scanner in = new Scanner(fr);
+            while (in.hasNextLine()) {
+                line = in.nextLine();
+                //remove all non numeric characters with regex
+                numericLine = line.replaceAll("[^0-9]", "");
+                //sum first and last number
+                if (numericLine.length() > 1) {
+                    System.out.println("numbers from line " + lineCounter +": " + numericLine.substring(0, 1) + " " + numericLine.substring(numericLine.length() - 1));
+                    System.out.println(numericLine.substring(0, 1) + numericLine.substring(numericLine.length() - 1));
+                    sum += Integer.parseInt(numericLine.substring(0, 1) + numericLine.substring(numericLine.length() - 1));
+                    }
+                else if (numericLine.length() == 1) {
+                    System.out.println("numbers from line " + lineCounter + ": " + numericLine.substring(0, 1));
+                    sum += Integer.parseInt(numericLine.substring(0, 1) + numericLine.substring(0, 1));
+                    }
+                else {
+                    sum += 0;
+                }
+                System.out.println("Line " + lineCounter + " sum: " + sum);
+                lineCounter++;
+            }
+            System.out.println("Answer: " + sum);
+            in.close();
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
 }
